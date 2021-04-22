51bc9fce4d173ef325482334a943ee474a9bc2b0
JieShengNP <s10205049@connect.np.edu.sg>
2021-04-22 10:36:17 +0800

    Complete Question4.java

--- Question4.java
+++ Question4.java
@@ -18,6 +18,13 @@ public class Question4
      */
      
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter the base of the triangle: ");
+    int number = in.nextInt();
+    for (int i = number; i > 0; i--){
+      for (int j = 0; j < i; j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
 }
