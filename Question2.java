15ef6828091390301f3cdb576ee94386e52d2d67
JieShengNP <s10205049@connect.np.edu.sg>
2021-04-22 12:19:12 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -11,10 +11,11 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
-    System.out.print("Enter your height(m): ");
+    //System.out.print("Enter your height(m): ");
     double height = in.nextDouble();
-    System.out.print("Enter your weight(kg): ");
+    //System.out.print("Enter your weight(kg): ");
     double weight = in.nextDouble();
-    System.out.println("Your BMI is " + String.format("%.2f", (weight/(height*height))));
+    //System.out.println("Your BMI is " + String.format("%.2f", (weight/(height*height))));
+    System.out.println((weight/(height*height)));
   }
 }
